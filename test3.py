08f44844a161df6ffde6a91adc9aabcdc1144c26
ChunHeiWongIvan <ivanwongcky@gmail.com>
2025-03-24 03:04:42 +0000

    multiple sources (cleaner commit)

--- test3.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# Initialize a 3x3 array with random values from a standard normal distribution
-arr = np.random.randn(3, 3)
-print(arr)
-
-# Initialize a 3x3 array with values from a normal distribution with mean 0 and standard deviation 1
-arr = np.random.normal(0, 1, (4, 3))
-print(arr)
